8d7fe822534fe396d1b02907e6b84ecedff33fdd
Your Name <admin@orsys.fr>
2022-05-04 13:27:26 +0100

    ajout de tests

---
+++ monappli-web/src/test/java/fr/madacamia/app/TestWeb.java
@@ -0,0 +1,34 @@
+package fr.madacamia.app;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class TestWeb {
+
+	@Test
+	public void test01() {
+		assertTrue(true);
+	}
+	
+	@Test
+	public void test02() {
+		assertTrue(true);
+	}
+	
+	@Test
+	public void test03() {
+		assertTrue(true);
+	}
+	
+	@Test
+	public void test04() {
+		assertTrue(true);
+	}
+	
+	@Test
+	public void test05() {
+		assertTrue(true);
+	}
+
+}
